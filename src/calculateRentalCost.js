53fc687b593c522c4dba10d88e21a0951bb85f35
Níck <nicolasiesus10@gmail.com>
2025-12-27 18:25:07 -0300

    exercise completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,29 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let discountValue = 0;
+
+  const baseValue = 40;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const lowTerm = 3;
+  const lowTermDiscount = 20;
+
+  if (days >= longTerm) {
+    discountValue = days * baseValue - longTermDiscount;
+
+    return discountValue;
+  }
+
+  if (days >= lowTerm) {
+    discountValue = days * baseValue - lowTermDiscount;
+
+    return discountValue;
+  }
+
+  discountValue = days * baseValue;
+
+  return discountValue;
 }
 
 module.exports = calculateRentalCost;
